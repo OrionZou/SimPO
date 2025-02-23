573a0ea0a99bf7b9ae14b75cb413742f36131b30
orion.zou <zgy@122066329@163.com>
2025-02-23 10:02:54 +0000

    update

--- run_train.sh
+++ run_train.sh
@@ -1,4 +1,4 @@
-# export all_proxy=http://127.0.0.1:1081
+export all_proxy=http://127.0.0.1:1081
 
 export HF_ENDPOINT=https://hf-mirror.com
 export HF_HOME=/tf/orion.zou/huggingface
@@ -11,9 +11,14 @@ export PYTHONPATH=.:$PYTHONPATH
 # CUDA_VISIBLE_DEVICES=3,7 nohup accelerate launch --config_file accelerate_configs/deepspeed_zero3.yaml --main_process_port 29501 --num_processes 2 scripts/run_simpo.py training_configs/llama-3-8b-instruct-simpo-aflow_v3_2.yaml  > llama-3-8b-instruct-simpo-alow-3.log 2>&1 &
 
 # CUDA_VISIBLE_DEVICES=7 nohup accelerate launch --config_file accelerate_configs/deepspeed_zero3.yaml --main_process_port 29501 --num_processes 1 scripts/run_simpo.py training_configs/llama-3-8b-instruct-simpo-aflow_v3_2.yaml  > llama-3-8b-instruct-simpo-alow-3_1.log 2>&1 &
-
+# CUDA_VISIBLE_DEVICES=4,5 nohup accelerate launch --config_file accelerate_configs/deepspeed_zero3.yaml --main_process_port 29501 --num_processes 2 scripts/run_simpo.py training_configs/llama-3-8b-instruct-simpo-aflow_v6.yaml  > llama-3-8b-instruct-simpo-alow-6.log 2>&1 &
+export PYTORCH_CUDA_ALLOC_CONF="expandable_segments:True"
+# CUDA_VISIBLE_DEVICES=3,4 nohup accelerate launch --config_file accelerate_configs/deepspeed_zero3.yaml --main_process_port 29501 --num_processes 2 scripts/run_simpo.py training_configs/llama-3-8b-instruct-simpo-aflow_v3_2_onlycot.yaml  > llama-3-8b-instruct-simpo-alow-3-onlycot.log 2>&1 &
+# CUDA_VISIBLE_DEVICES=3,4 nohup accelerate launch --config_file accelerate_configs/deepspeed_zero3.yaml --main_process_port 29501 --num_processes 2 scripts/run_simpo.py training_configs/llama-3-8b-instruct-simpo-aflow_v3_2_graph_cot.yaml  > llama-3-8b-instruct-simpo-alow-3-graph+cot.log 2>&1 &
+# CUDA_VISIBLE_DEVICES=6,7 nohup accelerate launch --config_file accelerate_configs/deepspeed_zero3.yaml --main_process_port 29502 --num_processes 2 scripts/run_simpo.py training_configs/llama-3-8b-instruct-simpo-aflow_v5.yaml  > llama-3-8b-instruct-simpo-alow-5.log 2>&1 &
+# CUDA_VISIBLE_DEVICES=4,5 nohup accelerate launch --config_file accelerate_configs/deepspeed_zero3.yaml --main_process_port 29501 --num_processes 2 scripts/run_simpo.py training_configs/llama-3-8b-instruct-simpo-aflow_v7.yaml  > llama-3-8b-instruct-simpo-alow-7.log 2>&1 &
 # CUDA_VISIBLE_DEVICES=4  torchrun --master_port 29501 --nproc_per_node 1 scripts/run_simpo.py training_configs/llama-3-8b-instruct-simpo-aflow_v3_2.yaml 
-CUDA_VISIBLE_DEVICES=3  nohup accelerate launch --config_file accelerate_configs/deepspeed_zero3.yaml --main_process_port 29501  --num_processes 1 scripts/run_simpo.py training_configs/llama-3-8b-instruct-simpo-aflow_v3_2_eval.yaml > llama-3-8b-instruct-simpo-alow-3_2_eval.log 2>&1 &
+# CUDA_VISIBLE_DEVICES=3  nohup accelerate launch --config_file accelerate_configs/deepspeed_zero3.yaml --main_process_port 29501  --num_processes 1 scripts/run_simpo.py training_configs/llama-3-8b-instruct-simpo-aflow_v3_2_eval.yaml > llama-3-8b-instruct-simpo-alow-3_2_eval.log 2>&1 &
 
 
 # CUDA_VISIBLE_DEVICES=2,3,4,5 nohup accelerate launch --config_file accelerate_configs/deepspeed_zero3.yaml scripts/run_simpo.py training_configs/llama-3-8b-instruct-simpo-aflow.yaml > llama-3-8b-instruct-simpo-alow-2.log 2>&1 &
@@ -23,5 +28,5 @@ CUDA_VISIBLE_DEVICES=3  nohup accelerate launch --config_file accelerate_configs
 # CUDA_VISIBLE_DEVICES=2,3,4,5 nohup accelerate launch --config_file accelerate_configs/deepspeed_zero3.yaml --main_process_port 29501 --num_processes 4 scripts/run_simpo.py training_configs/llama-3-8b-instruct-simpo-codeUltrafeedback.yaml  > llama-3-8b-instruct-simpo-codeUltrafeedback.log 2>&1 &
 
 # python test_wandb.py
-# wandb sync  /tf/orion.zou/repos/SimPO/wandb/offline-run-20250116_090552-91d0pv2c
+wandb sync /tf/orion.zou/repos/SimPO/wandb/offline-run-20250218_151113-go8fsdnw
 
--- serving_scripts/serving.sh
+++ serving_scripts/serving.sh
@@ -1,16 +1,17 @@
 
-# CUDA_VISIBLE_DEVICES=2 nohup vllm serve /tf/orion.zou/repos/SimPO/outputs/llama-3-8b-instruct-simpo-aflow-3 --dtype auto --api-key token-abc123 --port 8085 --host 0.0.0.0 --gpu_memory_utilization 0.95  --seed 1234  --max-model-len 8192  > vllm_serving.log 2>&1 &
-
-# CUDA_VISIBLE_DEVICES=2 nohup vllm serve /tf/orion.zou/repos/SimPO/outputs/llama-3-8b-instruct-simpo-aflow-3 --dtype auto --task reward --api-key token-abc123 --port 8082 --host 0.0.0.0 --gpu_memory_utilization 0.95  --seed 1234 > vllm_serving.log 2>&1 &
-
-
-# CUDA_VISIBLE_DEVICES=2 nohup python serving_scripts/vllm_serving.py > vllm_serving.log 2>&1 &
-
-# CUDA_VISIBLE_DEVICES=3 nohup python serving_scripts/rm_serving.py --model /tf/orion.zou/repos/SimPO/outputs/llama-3-8b-instruct-simpo-aflow-3 --port 8086 --host 0.0.0.0 --max_batch_size 32  > rm_serving.log 2>&1 &
-CUDA_VISIBLE_DEVICES=3 nohup python serving_scripts/rm_serving.py --model /tf/model/Llama3/Meta-Llama-3-8B-Instruct --port 8086 --host 0.0.0.0 --max_batch_size 32  > rm_serving.log 2>&1 &
-
-# CUDA_VISIBLE_DEVICES=3  python serving_scripts/test_rm_serving.py
+CUDA_VISIBLE_DEVICES=3 nohup vllm serve /tf/orion.zou/repos/SimPO/outputs/llama-3-8b-instruct-simpo-aflow-3 --dtype auto --api-key token-abc123 --port 8085 --host 0.0.0.0 --gpu_memory_utilization 0.95 --tensor-parallel-size 1  --seed 1234  --max-model-len 8192 --max-model-len 8192 --block-size 32 --max-num-seqs 1024 --max-num-batched-tokens 8192   > vllm_serving_8085.log 2>&1 &
+# CUDA_VISIBLE_DEVICES=6 nohup vllm serve /tf/orion.zou/repos/SimPO/outputs/llama-3-8b-instruct-simpo-aflow-v6 --dtype auto --api-key token-abc123 --port 8092 --host 0.0.0.0 --gpu_memory_utilization 0.95 --tensor-parallel-size 1  --seed 1234  --max-model-len 8192 --block-size 32 --max-num-seqs 1024 --max-num-batched-tokens 8192  > vllm_serving_8092.log 2>&1 &
 
 # export VLLM_ENGINE_ITERATION_TIMEOUT_S=3600
 # CUDA_VISIBLE_DEVICES=2 python serving_scripts/vllm_serving.py --model /tf/orion.zou/repos/SimPO/outputs/llama-3-8b-instruct-simpo-aflow-3 --port 8085 --host 0.0.0.0   > vllm_serving.log 2>&1 &
 # CUDA_VISIBLE_DEVICES=2 python serving_scripts/vllm_serving.py --model /tf/orion.zou/repos/SimPO/outputs/llama-3-8b-instruct-simpo-aflow-3 --port 8085 --host 0.0.0.0 
+
+
+
+# CUDA_VISIBLE_DEVICES=3 nohup python serving_scripts/rm_serving.py --model /tf/orion.zou/repos/SimPO/outputs/llama-3-8b-instruct-simpo-aflow-3 --port 8086 --host 0.0.0.0 --max_batch_size 32  > rm_serving.log 2>&1 &
+
+# CUDA_VISIBLE_DEVICES=4 nohup python serving_scripts/rm_serving.py --model /tf/orion.zou/repos/SimPO/outputs/llama-3-8b-instruct-simpo-aflow-3 --port 8086 --host 0.0.0.0 --max_batch_size 32  > rm_serving_8086.log 2>&1 &
+# CUDA_VISIBLE_DEVICES=7 nohup python serving_scripts/rm_serving.py --model /tf/orion.zou/repos/SimPO/outputs/llama-3-8b-instruct-simpo-aflow-v6 --port 8093 --host 0.0.0.0 --max_batch_size 32  > rm_serving_8093.log 2>&1 &
+
+
+# CUDA_VISIBLE_DEVICES=4 nohup python serving_scripts/rm_serving.py --model /tf/model/Llama3/Meta-Llama-3-8B-Instruct --port 8086 --host 0.0.0.0 --max_batch_size 32  > rm_serving.log 2>&1 &
